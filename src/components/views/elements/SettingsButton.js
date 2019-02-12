721b4faa698eaf6ece093fbbc7c7e10ffff52f81
Léo Mora <yoshin@live.fr>
2019-02-12 13:36:58 +0100

    Update the app icons

--- src/components/views/elements/SettingsButton.js
+++ src/components/views/elements/SettingsButton.js
@@ -24,7 +24,7 @@ const SettingsButton = function(props) {
     return (
         <ActionButton action="view_user_settings"
             label={_t("Settings")}
-            iconPath="img/icons-settings.svg"
+            iconPath="img/tc_icons-settings.svg"
             size={props.size}
             tooltip={props.tooltip}
         />
